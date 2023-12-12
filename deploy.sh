1a6fb76b4fad5e9e42a077e19689c3f5500fab9c
joebentley <joebentley10@gmail.com>
2023-12-12 12:52:53 +0100

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,8 @@ PATH_PREFIX="/blog/" npx @11ty/eleventy || { echo 'build failed' ; exit 1; }
 rm -r ~/Site/blog
 cp -r _site ~/Site/blog
 
+cd ~/Site
+git add .
+git commit -m "Updated blog"
+
 echo 'Now go and push it!'
